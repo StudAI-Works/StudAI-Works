ae8252fc2d631bf84ca014168cd23cdfe219b5ef
damodara2006 <damodara2006@gmail.com>
2025-06-29 11:02:43 +0530

    feat : added react-toastify

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useRef } from 'react'
 import { IoIosArrowRoundUp } from "react-icons/io";
-
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css"
 function Home() {
   const BASE_URL: string = "http://localhost:8080"
   const userPromt = useRef<HTMLTextAreaElement | null>(null)
@@ -10,18 +11,25 @@ function Home() {
       axios.post(`${BASE_URL}/userpromt`, { Promt: userPromt.current.value })
         .then(res => {
           console.log(res)
+          if (res.data) {
+            toast.success("Sent", {autoClose:1500, style:{fontSize:"14px", width:"140px"}})
+          }
+          else {
+            toast.error("Faild",{autoClose:1500})
+          }
         })
     }
   }
   return (
     <div className='text-3xl relative w-screen min-h-screen overflow-y-auto'>
+      <ToastContainer/>
       <p className='text-lg'>Home part goes here</p>
 
       <div></div>
 
       <div className='w-[50%]  absolute bottom-0 transition-all    flex items-center justify-evenly pl-10  h-20 '>
-        <textarea className='border border-gray-100 placeholder:text-sm focus:bg-gray-100 rounded-2xl focus:outline-1 pt-3  bottom-10 w-[70%]  placeholder:top-2 md:placeholder:top-3 min-h-11 text-sm pl-4 placeholder:absolute lg:placeholder:top-3 xl:placeholder:top-3' placeholder='Enter your promt here' ref={userPromt} />
-        <button className='border border-gray-200 rounded-3xl  bottom-10 hover:bg-gray-200 transition-all duration-100 right-0 p-1 active:scale-125'><IoIosArrowRoundUp className='font-light' onClick={HandlePromt} /></button>
+        <textarea className='border border-gray-100 placeholder:text-sm left-4 w-[46%] absolute top-5 focus:bg-gray-100 rounded-2xl focus:outline-0 pt-3  bottom-10   placeholder:top-2 md:placeholder:top-3 min-h-11 text-sm pl-4 placeholder:absolute lg:placeholder:top-3 xl:placeholder:top-3' placeholder='Enter your promt here' ref={userPromt} />
+        <button className='border border-gray-200 mt-1 rounded-3xl  bottom-10 hover:bg-gray-200 transition-all duration-100 right-0 p-1 ml-2 active:scale-125'><IoIosArrowRoundUp className='font-light' onClick={HandlePromt} /></button>
       </div>
     </div>
   )
